a2504bd4b3dd6f5c2f0cfeb519439d29a487861a
larissa-NUF <larissanuf.figueredo@gmail.com>
2021-02-24 19:33:53 -0300

    primeiros projetos em c#

---
+++ HelloWorld/HelloWorld/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            /*
+            int natal_dia = 25;
+            string cidade = "Ribeirão Pires";
+            float peso = 53.600f;
+            bool homem = false;
+
+
+
+            Console.WriteLine("Hello World");
+            Console.WriteLine(natal_dia);
+            Console.WriteLine(cidade);
+            Console.WriteLine(peso);
+            Console.WriteLine(homem);
+            Console.WriteLine("Tooooooooooop");
+
+            homem = true;
+
+            Console.WriteLine(homem);
+            */
+
+            /*
+            var numero = 45;
+            var palavra = "legal";
+            */
+
+            dynamic caminho = "vermelho";
+
+            Console.WriteLine(caminho);
+
+            caminho = 50;
+
+            Console.WriteLine(caminho);
+
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ idade/idade/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace idade
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Digite sua idade:");
+            int idade = int.Parse(Console.ReadLine());
+                
+            if (idade >= 0 && idade <= 11)
+            {
+
+                Console.WriteLine("Você é uma criança.");
+
+            }else if (idade >=12 && idade<= 18)
+            {
+
+                Console.WriteLine("Você é uma adolescente.");
+
+            }else if (idade >= 19 && idade <= 50)
+            {
+                Console.WriteLine("Você é uma adulto.");
+            }
+            else
+            {
+                Console.WriteLine("Você é uma idoso.");
+            }
+            
+
+
+            Console.ReadLine();
+        }
+    }
+}
